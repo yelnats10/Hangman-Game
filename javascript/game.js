00bd210f9b07e01c365b77fe9107053e63199e3d
yelnats10 <yelnats10@gmail.com>
2017-08-28 23:12:18 -0400

    added some JS

--- javascript/game.js
+++ javascript/game.js
@@ -1,50 +1,48 @@
- // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
-    var computerChoices = ["r", "p", "s"];
 
-    // Creating variables to hold the number of wins, losses, and ties. They start at 0.
-    var wins = 0;
-    var losses = 0;
-    var ties = 0;
+var list = ["abc", "defg", "hijkl"];
 
-    // This function is run whenever the user presses a key.
-    document.onkeyup = function(event) {
+var pickedList = list[Math.floor(Math.random() * list.length)];
 
-      // Determines which key was pressed.
-      var userGuess = event.key;
+var guess = [];
 
-      // Randomly chooses a choice from the options array. This is the Computer's guess.
-      var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-      // Reworked our code from last step to use "else if" instead of lots of if statements.
+for (var i = 0; i < pickedList.length; i++) {
+    guess.push(pickedList.charAt(i));
+}
 
-      // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-      if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
 
-        if ((userGuess === "r") && (computerGuess === "s")) {
-          wins++;
-        } else if ((userGuess === "r") && (computerGuess === "p")) {
-          losses++;
-        } else if ((userGuess === "s") && (computerGuess === "r")) {
-          losses++;
-        } else if ((userGuess === "s") && (computerGuess === "p")) {
-          wins++;
-        } else if ((userGuess === "p") && (computerGuess === "r")) {
-          wins++;
-        } else if ((userGuess === "p") && (computerGuess === "s")) {
-          losses++;
-        } else if (userGuess === computerGuess) {
-          ties++;
-        }
+var got = [];
 
-        // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
-        var html =
-          "<p>You chose: " + userGuess + "</p>" +
-          "<p>The computer chose: " + computerGuess + "</p>" +
-          "<p>wins: " + wins + "</p>" +
-          "<p>losses: " + losses + "</p>" +
-          "<p>ties: " + ties + "</p>";
+    
+document.onkeyup = function(event) {
 
-        // Set the inner HTML contents of the #game div to our html string
-        document.querySelector("#hangman").innerHTML = html;
-      }
-    };
+ var userGuess = event.key;
+
+    
+  if (pickedList === "defg") {
+
+    if ((userGuess === guess[0]) || (userGuess === guess[1]) || (userGuess === guess[2]) || (userGuess === guess[3])) {
+
+      if (userGuess === guess[0]) {
+        got.push(userGuess);
+      } 
+      else if (userGuess === guess[1]) {
+        got.push(userGuess);
+      } 
+      else if (userGuess === guess[2]) {
+        got.push(userGuess);
+      } 
+      else if (userGuess === guess[3]) {
+        got.push(userGuess);
+      } 
+    
+
+       
+      var html =
+        "<p>" + got[0] + " " + got[1] + " " + got[2] + " " + got[3] + "</p>";
+
+          // Set the inner HTML contents of the #game div to our html string
+      document.querySelector("#hangman").innerHTML = html;
+    }
+  }
+};
