ca8a2b27ffc4e46f87dd5b06262c462926741e2f
yelnats10 <yelnats10@gmail.com>
2017-08-30 23:50:53 -0400

    added a few if statements and videos and replace arrays

--- javascript/game.js
+++ javascript/game.js
@@ -1,48 +1,88 @@
 
-var list = ["abc", "defg", "hijkl"];
+//list to guess
 
+var list = ["watchmen", "frozen", "cars","taken","twins","scream","superbad"];
+
+
+//random pick one of the items in the list
 var pickedList = list[Math.floor(Math.random() * list.length)];
 
+
+//to take the random pickedList and break up each letter
 var guess = [];
 
 
 for (var i = 0; i < pickedList.length; i++) {
     guess.push(pickedList.charAt(i));
-}
+
+};
+
+//test to see if guess worked
+console.log(guess);
 
 
-var got = [];
 
-    
+var replaceMe = [];
+
+for (var i = 0; i < guess.length; i++) {
+    //push to an array
+    // document.getElementById("hangman-good").innerHTML += "_ ";
+    replaceMe.push("_ ");
+  };
+
+console.log(replaceMe);
+
+var wrong = [];
+
 document.onkeyup = function(event) {
 
  var userGuess = event.key;
+ var movies = "<p>Guess the Movie</p>"; 
+ document.querySelector("#subject").innerHTML = movies;
+ 
 
-    
-  if (pickedList === "defg") {
+ if (guess.indexOf(userGuess) < 0) {
+      wrong.push(userGuess);
 
-    if ((userGuess === guess[0]) || (userGuess === guess[1]) || (userGuess === guess[2]) || (userGuess === guess[3])) {
+      var targetBad = document.getElementById("hangman-bad");
+      targetBad.innerHTML = wrong;
 
-      if (userGuess === guess[0]) {
-        got.push(userGuess);
-      } 
-      else if (userGuess === guess[1]) {
-        got.push(userGuess);
-      } 
-      else if (userGuess === guess[2]) {
-        got.push(userGuess);
-      } 
-      else if (userGuess === guess[3]) {
-        got.push(userGuess);
-      } 
-    
+    } else {
+      var mix = guess.indexOf(userGuess);
+      replaceMe[mix] = userGuess;
+      document.getElementById("hangman-good").innerHTML = replaceMe.join("");
 
-       
-      var html =
-        "<p>" + got[0] + " " + got[1] + " " + got[2] + " " + got[3] + "</p>";
+        if (replaceMe.join("") === list[0]) {
 
-          // Set the inner HTML contents of the #game div to our html string
-      document.querySelector("#hangman").innerHTML = html;
+            var trailer = '<iframe width="560" height="315" src="https://www.youtube.com/embed/NUjMO_k9IF8?autoplay=1" frameborder="0" allowfullscreen></iframe>';
+            document.querySelector("#trailer").innerHTML = trailer;
+        } else if (replaceMe.join("") === list[1]) {
+
+            var trailer = '<iframe width="560" height="315" src="https://www.youtube.com/embed/TbQm5doF_Uc?autoplay=1" frameborder="0" allowfullscreen></iframe>';
+            document.querySelector("#trailer").innerHTML = trailer;
+        } else if (replaceMe.join("") === list[2]) {
+
+            var trailer = '<iframe width="560" height="315" src="https://www.youtube.com/embed/SbXIj2T-_uk?autoplay=1" frameborder="0" allowfullscreen></iframe>';
+            document.querySelector("#trailer").innerHTML = trailer;
+        } else if (replaceMe.join("") === list[3]) {
+
+            var trailer = '<iframe width="560" height="315" src="https://www.youtube.com/embed/uPJVJBm9TPA?autoplay=1" frameborder="0" allowfullscreen></iframe>';
+            document.querySelector("#trailer").innerHTML = trailer;
+        } else if (replaceMe.join("") === list[4]) {
+
+            var trailer = '<iframe width="560" height="315" src="https://www.youtube.com/embed/qtlB7q96NMs?autoplay=1" frameborder="0" allowfullscreen></iframe>';
+            document.querySelector("#trailer").innerHTML = trailer;
+        } else if (replaceMe.join("") === list[5]) {
+
+            var trailer = '<iframe width="560" height="315" src="https://www.youtube.com/embed/AWm_mkbdpCA?autoplay=1" frameborder="0" allowfullscreen></iframe>';
+            document.querySelector("#trailer").innerHTML = trailer;
+        } else if (replaceMe.join("") === list[6]) {
+
+            var trailer = '<iframe width="560" height="315" src="https://www.youtube.com/embed/4q5Mi9gWX8c?autoplay=1" frameborder="0" allowfullscreen></iframe>';
+            document.querySelector("#trailer").innerHTML = trailer;
+        } 
+
     }
-  }
+
+
 };
